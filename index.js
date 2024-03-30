e31be4cba78a5d9053eb2ab585a7bc8e1ce954ca
jarvis311 <jigneshpatel754622@gmail.com>
2024-03-30 15:10:41 +0530

    chnges

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const mainRouter = require('./src/routes/main.routes.js')
 require('dotenv').config()
 
 const app = express()
-
+app.get('/home',(req,res) => {
+    res.send(`<h1>Hello Ubuntu </h1>`)
+})
 app.use('/api',mainRouter)
 app.listen(process.env.PORT, () => {
     console.log("Server is connected")
